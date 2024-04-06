27fba2868aae0f0676089c49a52185141657b31a
Snehal Dhindle <130338423+SnehalDhindle@users.noreply.github.com>
2024-04-06 12:34:43 +0530

    finding max frequency character using Map

--- day26.java
+++ day26.java
@@ -1,36 +1,36 @@
+import java.util.*;
 class Day26 {
+    static void maxCharInString(String s){
+        s=s.toLowerCase().replace(" ", "");
+        System.out.println(s);
 
-    int majorityElement(int[] num){
+        Map<Character, Integer> hm = new HashMap<>();
 
-        int count = 0;
-        int ele = 0;
-
-        for(int i=0;i<num.length;i++){
-            if(count==0){
-                ele = num[i];
-                count++;
-            }
-            else{
-                if(num[i]==ele)
-                    count++;
-                else
-                    count--;
+        for(int i=0;i<s.length();i++){
+            if(!hm.containsKey(s.charAt(i))){
+                hm.put(s.charAt(i),1);
+            }else{
+                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
             }
         }
 
-        return ele;
+        int max = Integer.MIN_VALUE;
+        char maxchar = '\0';
+
+        for(Map.Entry<Character,Integer> entry : hm.entrySet()){
+           if(entry.getValue()>max){
+                max = entry.getValue();
+                maxchar = entry.getKey();
+           }
+        }
+
+        System.out.println(maxchar);
+        System.out.println(max);
+        
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String s = "Snehal Shivaji Dhindle";
 
-        Day26 d26 = new Day26();
-
-        int[] num1 = new int[]{3,2,3};
-        int[] num2 = new int[]{2,2,1,1,1};
-
-        int ret1 = d26.majorityElement(num1);
-        int ret2 = d26.majorityElement(num2);
-
-        System.out.println("1]" + ret1);
-        System.out.println("2]" + ret2);
-    }
+        maxCharInString(s);
+    }    
 }
